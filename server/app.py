b5b88ea181851a9e21005a7bddcad1213ba49916
Wambui <wambuikaranja78@gmail.com>
2025-09-23 23:13:27 +0300

    creates post request on users

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response
+from flask import Flask, make_response,request
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -29,7 +29,22 @@ class UserResource(Resource):
         users = User.query.all()
         return [user.to_dict() for user in users], 200
     
-    
+    def post(self):
+      data=request.get_json()
+
+      if not data.get("username") or not data.get("email") or not data.get("password_hash"):
+            return {"error": "username, email and password_hash are required"}, 400
+
+      new_user=User(
+          username=data.get('username'),
+          email=data.get('email'),
+          password_hash=data.get("password_hash")
+      )
+      db.session.add(new_user)
+      db.session.commit()
+      return new_user.to_dict(), 201 
+
+
 # Register the resource
 api.add_resource(UserResource, "/api/users")
 
