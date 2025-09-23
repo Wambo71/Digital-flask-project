4b70bb9d1ecbb5559845bf3b379ad6d39038894a
Wambui <wambuikaranja78@gmail.com>
2025-09-23 22:54:37 +0300

    creates get users route

--- server/app.py
+++ server/app.py
@@ -1,23 +1,38 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_cors import CORS
-from extensions import db, migrate, ma
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from extensions import db, migrate
 from config import Config
 from models import User, Product, Order, Review
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Initialize extensions here
+# Initialize extensions
 db.init_app(app)
 migrate.init_app(app, db)
-ma.init_app(app)
+api = Api(app)
 
-# Enable CORS (for React frontend requests)
+# Enable CORS
 CORS(app)
+
 
 @app.route("/")
 def hello():
     return "<h1>Welcome to backend</h1>"
+
+
+
+class UserResource(Resource):
+    def get(self):
+        users = User.query.all()
+        return [user.to_dict() for user in users], 200
+    
+    
+# Register the resource
+api.add_resource(UserResource, "/api/users")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5500)
