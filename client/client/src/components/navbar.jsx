1533df2cc5037eb21c3375e7e31f9ecf2bd1da51
Wambui <wambuikaranja78@gmail.com>
2025-09-24 15:21:24 +0300

    creates navbar

---
+++ client/client/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/products">Products</NavLink>
+      <NavLink to="/cart">Cart</NavLink>
+      <NavLink to="/Login_page">Login</NavLink>
+      <NavLink to="/profile">Profile</NavLink>
+    </nav>
+  );
+}
+
+export default NavBar;
+
