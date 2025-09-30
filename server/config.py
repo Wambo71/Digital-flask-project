ca739025bd22365538967120ed48b9a75ef8b457
Eddah kibet <eddahkibet20@gmail.com>
2025-09-30 12:57:15 +0300

    config

--- server/config.py
+++ server/config.py
@@ -1,6 +1,5 @@
-
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # Provided by Render
+    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"  # Provided by Render
     SQLALCHEMY_TRACK_MODIFICATIONS = False
